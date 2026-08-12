00da80f2c6166c913ae0a132df250f5b68af431f
Waqas Ali <waqasansari4547@gmail.com>
2026-08-12 22:14:13 +0500

    Create DB

---
+++ Postgre SQL/part1/01_first-database.sql
@@ -0,0 +1,7 @@
+
+-- you should never do this in production app
+-- this is only for learning
+DROP DATABASE IF EXISTS postgresql_part1;
+
+-- create new db inside your postgre server
+CREATE DATABASE postgresql_part1;
