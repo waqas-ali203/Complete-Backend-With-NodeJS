7066596f64b61ec6e95aff9741f63b2a00060550
Waqas Ali <waqasansari4547@gmail.com>
2026-08-13 18:04:31 +0500

    Data Types

---
+++ Postgre SQL/part1/04_data_types.sql
@@ -0,0 +1,44 @@
+
+
+DROP TABLE IF EXISTS basics.products_basic;
+
+CREATE TABLE basics.products_basic (
+
+    id SERIAL PRIMARY KEY,
+
+    -- string -> max length of 100 chars
+    name VARCHAR(100) NOT NULL,
+
+    description TEXT,
+
+    STOCK INTEGER DEFAULT 0,
+
+    -- store larger whole number than INTEGER
+    total_views BIGINT DEFAULT 0,
+
+    -- exact decimal values
+    -- 10 means total digist
+    -- 2 maens digist after the decimal point , 99999.99
+    price NUMERIC(10,2),
+
+    is_active BOOLEAN DEFAULT true
+);
+
+-- queries
+
+INSERT INTO basics.products_basic
+(name, description, stock, total_views, price, is_active)
+VALUES 
+(
+    'product 1',
+    'product desc',
+    100,
+    1200,
+    2455.45,
+    true
+);
+SELECT * FROM basics.products_basic;
+
+SELECT id, name, price , is_active
+FROM basics.products_basic
+WHERE is_active
---
+++ Postgre SQL/part1/05_other_data_types.sql
@@ -0,0 +1,35 @@
+
+
+DROP TABLE IF EXISTS basics.app_events;
+
+CREATE TABLE basics.app_events (
+
+    -- UUID
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    event_name TEXT NOT NULL,
+
+    -- JSONB
+    metadata JSONB DEFAULT '{}':: jsonb,
+
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+INSERT INTO basics.app_events (event_name, metadata)
+VALUES
+(
+    'sign_up',
+    '{"browser": "chrome"}'
+),
+(
+        'sign_in',
+    '{"user": "waqas"}'
+);
+
+-- SELECT * FROM basics.app_events;
+
+SELECT 
+event_name,
+metadata ->> 'browser' AS browser
+FROM basics.app_events
+WHERE metadata ? 'browser';
